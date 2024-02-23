92fc37ea933f294c20e074ba29c7f3989b3732bf
Henry Langenhoven <henrylangenhoven8@gmail.com>
2024-02-23 11:06:22 +0100

    added install script

---
+++ scripts/aptInstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt upgrade -y
+sudo apt autoremove -y
+
+apt_file="/home/henry/dotfiles/scripts/.apt"
+
+# Sort the file and remove duplicates
+sort "$apt_file" | uniq > "$apt_file".tmp
+
+# Replace the original file with the sorted one
+mv "$apt_file".tmp "$apt_file"
+
+# Initialize an empty string to hold the package names
+packages=""
+
+# Read the .apt file line by line
+while IFS= read -r line
+do
+  # Concatenate the package names
+  packages="$packages $line"
+done < "$apt_file"
+
+# Run the apt install command with the package names
+sudo apt install -y $packages
