7aff092225e61a73eaed8e014068906b0fe16f32
Henry Langenhoven <henry.langenhoven.dev@gmail.com>
2024-07-05 08:41:05 +0200

    added delete_empty_trader_namespaces.sh

---
+++ entrnce/delete_empty_trader_namespaces.sh
@@ -0,0 +1,19 @@
+# Set the context to the test cluster
+kubectl config use-context tst
+
+# Get all namespaces that start with 'trader-'
+namespaces=$(kubectl get ns --no-headers -o custom-columns=":metadata.name" | grep '^trader-')
+
+# Iterate through each namespace individually
+echo "$namespaces" | while read -r ns; do
+  # Check if there are any running pods in the namespace
+  running_pods=$(kubectl get pods -n $ns --field-selector=status.phase=Running --no-headers)
+
+  # If there are no running pods, delete the namespace
+  if [ -z "$running_pods" ]; then
+    echo "Deleting namespace: $ns"
+    kubectl delete ns $ns
+  else
+    echo "Namespace $ns has running pods, skipping..."
+  fi
+done
