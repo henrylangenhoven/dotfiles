cc43683848133df4ba86e1c7cebdb0bdfa8fef8f
Henry Langenhoven <henry.langenhoven.dev@gmail.com>
2024-07-29 11:32:20 +0200

    updated clearDownloads.sh

--- scripts/clearDownloads.sh
+++ scripts/clearDownloads.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Find and remove files older than 7 days in the Downloads directory
-find $HOME/Downloads -type f -mtime +7 -exec rm -f {} \;
+find $HOME/Downloads -type f -mtime +7 -exec rm -f {} \; 2>/dev/null
+
+# Find and remove empty directories in the Downloads directory
+find $HOME/Downloads -type d -empty -exec rmdir {} \; 2>/dev/null
