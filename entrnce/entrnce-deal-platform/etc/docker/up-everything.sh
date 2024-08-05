289050c20a88a1c1567ef7fbf01c9f49d3004a1d
Henry Langenhoven <henry.langenhoven.dev@gmail.com>
2024-08-05 15:31:18 +0200

    added down pull and up scripts for deal-platform

---
+++ entrnce/entrnce-deal-platform/etc/docker/up-everything.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.brp-adapter.yml -f docker-compose.robot-in-docker.yml --profile everything up -d
