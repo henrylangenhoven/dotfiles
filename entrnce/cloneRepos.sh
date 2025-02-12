87c905f09989d347d554342779f2fa7242bd4aab
Henry Langenhoven <henry.langenhoven@entrnce.com>
2025-02-12 14:15:35 +0100

    updated cloneRepos.sh for windows

--- entrnce/cloneRepos.sh
+++ entrnce/cloneRepos.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Path to your file
-repo_file="$DOTFILES/entrnce/.repos"
+repo_file="/d/lang/dotfiles/entrnce/.repos"
 
 # Destination folder for cloning (hardcoded)
-destination_folder="$ENTRNCE"
+destination_folder="/d/entrnce"
+username="henrylangenhoven"
 
-# Prompt for GitHub username and password
-read -r -p "Enter your GitHub username: " username
-read -r -s -p "Enter your GitHub password: " password
+# Prompt for GitHub token
+read -r -s -p "Enter your GitHub token: " token
 echo
 
 # Check if the file exists
@@ -19,18 +19,22 @@ fi
 
 # Function to clone a single repository
 clone_repo() {
-  local repo=$1
+  local repo="$1"
+  # Remove carriage returns if present
+  repo=$(echo "$repo" | tr -d '\r')
   local destination="$destination_folder/$repo"
 
   # Create destination folder if it doesn't exist
   mkdir -p "$destination"
 
   # Clone the repository into the specified destination folder
-  git clone "https://${username}:${password}@github.com/EnergyExchangeEnablersBV/$repo.git" "$destination"
+  echo "will now run: git clone \"https://${username}:${token}@github.com/EnergyExchangeEnablersBV/${repo}.git\" \"$destination\""
+  git clone "https://${username}:${token}@github.com/EnergyExchangeEnablersBV/${repo}.git" "$destination"
 }
 
 # Loop through each line in the file and clone repos concurrently
 while IFS= read -r repo || [[ -n "$repo" ]]; do
+  repo=$(echo "$repo" | tr -d '\r')
   clone_repo "$repo" &
 done < "$repo_file"
 
