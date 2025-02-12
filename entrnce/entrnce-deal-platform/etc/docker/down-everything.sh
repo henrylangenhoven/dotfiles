28702527de8392cd958241c5afc9fc239d8cfe4d
Henry Langenhoven <henry.langenhoven@entrnce.com>
2025-02-12 13:51:34 +0100

    removed unused stow files

--- entrnce/entrnce-deal-platform/etc/docker/down-everything.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.brp-adapter.yml -f docker-compose.robot-in-docker.yml --profile everything down
